85d7b7a80dfc32deff5de6913eda39243429d070
Mahan Mirdar <mahan.mirdar1381@gmail.com>
2025-01-12 00:46:40 +0330

    Configurations
    Part Of Configurations

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from blog import views
+
+app_name = "blog"
+urlpatterns = [
+    path("",views.blog_view, name="index"),
+    path("single",views.single_view, name="single"),
+]
--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from website import views
 
+app_name = "website"
 urlpatterns = [
-    path("",views.index_view),
-    path("contact/",views.contact_view),
-    path("about/",views.about_view),
+    path("",views.index_view, name="index"),
+    path("contact/",views.contact_view, name="contact"),
+    path("about/",views.about_view, name="about"),
 ]
