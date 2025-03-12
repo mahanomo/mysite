f72d93e897aa49e3b8c13b02821828c63b6507b2
Mahan Mirdar <mahan.mirdar1381@gmail.com>
2025-03-12 13:35:21 +0330

    create simple database

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=215)
     content = models.TextField()
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.TimeField(null= True)
+    created_date = models.TimeField(auto_now_add= True)
+    updated_date = models.TimeField(auto_now= True)
+    
--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=250)
+    message = models.TextField()
+    created_date = models.TimeField(auto_now_add=True)
+    updated_date = models.TimeField(auto_now=True)
