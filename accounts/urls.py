1fb4c8c0f8cfb35c472e0201e606167fc64a6194
Mahan Mirdar <mahan.mirdar1381@gmail.com>
2025-04-20 14:51:35 +0330

    django authentications set

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from accounts import views
+from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 urlpatterns = [
@@ -9,4 +10,10 @@ urlpatterns = [
     # logout
     path("signup/",views.signup_view, name="signup"),
     # signup
-    ]
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
+
+    
--- mysite/settings.py
+++ mysite/settings.py
@@ -161,3 +161,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     "127.0.0.1",
 #     # ...
 # ]
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+# settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.example.com'  # تنظیمات SMTP
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mahan.mirdar1381@gmail.com'
+EMAIL_HOST_PASSWORD = 'mahan.mirdar1381@gmail.com'
+DEFAULT_FROM_EMAIL = 'mahan.mirdar1381@gmail.com'
